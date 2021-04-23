8989569b5c03ab6e831b99c260cf01fbaf978087
dean <deanraguso@gmail.com>
2021-04-23 13:33:16 +1000

    Player loads in and collides with world

--- game.js
+++ game.js
@@ -9,7 +9,29 @@ const config = {
             gravity: { y: 200 },
             enableBod: true
         }
+    },
+    scene: {
+        preload,
+        create, 
+        update
     }
 }
+
+const gameState = {};
+
+function preload(){
+    this.load.image('player', 'assets/player.png');
+}
+
+function create(){
+    gameState.player = this.physics.add.sprite(250,250, 'player');
+    gameState.player.setCollideWorldBounds(true);
+}
+
+function update(){
+
+}
+
+
 
 const game = new Phaser.Game(config);
