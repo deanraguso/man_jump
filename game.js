97025972a682ef948055e72904a4b56dec2d1af4
dean <deanraguso@gmail.com>
2021-04-23 18:04:34 +1000

    randomly generated platforms working

--- game.js
+++ game.js
@@ -6,8 +6,8 @@ const config = {
     physics: {
         default: 'arcade',
         arcade: {
-            gravity: { y: 200 },
-            enableBod: true
+            gravity: { y: 500 },
+            // debug: true
         }
     },
     scene: {
@@ -24,11 +24,55 @@ function preload(){
 }
 
 function create(){
-    gameState.player = this.physics.add.sprite(250,250, 'player');
+    gameState.player = this.physics.add.sprite(250,550, 'player');
     gameState.player.setCollideWorldBounds(true);
+
+    gameState.cursors = this.input.keyboard.createCursorKeys();
+    
+    gameState.platform = []
+    gameState.p_inc = 70;
+    gameState.platforms = this.physics.add.staticGroup();
+
+    const createPlatform = (x,y) => {
+        gameState.platform.push(this.add.rectangle(x,y,70,10,'0xFFaa10'));
+        gameState.platforms.add(gameState.platform[gameState.platform.length-1]);
+    }
+
+    const displayHeight = 600;
+    const displayWidth = 600;
+
+    const buildPlatforms = () => {
+        const p_max = displayHeight / gameState.p_inc;
+        start_point = displayHeight - (gameState.platform.length + 1)*gameState.p_inc;
+
+        // Build from there.
+        for (let y = start_point; y > 0; y-= gameState.p_inc){
+            createPlatform(Math.abs(Math.round(Math.random()*displayWidth/2 - displayWidth/3)), y);
+        }
+    }
+
+    const lowerPlatforms = () => {
+        
+    }
+
+    this.physics.add.collider(gameState.player, gameState.platforms);
+    buildPlatforms();
 }
 
 function update(){
+    const speed = 150;
+
+    if(gameState.cursors.left.isDown){
+        gameState.player.setVelocityX(-speed);
+    } else if (gameState.cursors.right.isDown){
+        gameState.player.setVelocityX(speed);
+    } else {
+        gameState.player.setVelocityX(0);
+    }
+
+    if (gameState.cursors.space.isDown && gameState.player.body.blocked.down){
+        gameState.player.setVelocityY(-300);
+    }
 
 }
 
