add7d27135dc65bd5f830efde290141f614463e4
dean <deanraguso@gmail.com>
2021-04-23 19:46:22 +1000

    Fixed platform creation glitch, made platform generate past entry height

--- game.js
+++ game.js
@@ -30,7 +30,7 @@ function create(){
     gameState.cursors = this.input.keyboard.createCursorKeys();
     
     gameState.platform = []
-    gameState.p_inc = 70;
+    gameState.p_inc = 80;
     gameState.platforms = this.physics.add.staticGroup();
 
     const createPlatform = (x,y) => {
@@ -38,25 +38,50 @@ function create(){
         gameState.platforms.add(gameState.platform[gameState.platform.length-1]);
     }
 
-    const displayHeight = 600;
-    const displayWidth = 600;
+    const removeBottomPlatform = () => {
+        let oldStart = gameState.platform[0];
+        // let oldEnd = gameState.platform[gameState.platform.length-1];
+        // Must also remove the lowest platform from physical memory!
+        gameState.platform.shift();
 
-    const buildPlatforms = () => {
-        const p_max = displayHeight / gameState.p_inc;
-        start_point = displayHeight - (gameState.platform.length + 1)*gameState.p_inc;
+        // oldStart.destroy();
+        oldStart.destroy();
+        // console.log(gameState.platform.length);
+    }
+
+    gameState.displayHeight = game.config.height;
+    gameState.displayWidth = game.config.width; 
+
+    gameState.buildPlatforms = () => {
+        if (gameState.platform.length > 0){
+            start_point = gameState.displayHeight - gameState.platform.length* gameState.p_inc;
+        } else {
+            start_point = gameState.displayHeight - (gameState.platform.length + 1)*gameState.p_inc;
+        }
 
         // Build from there.
         for (let y = start_point; y > 0; y-= gameState.p_inc){
-            createPlatform(Math.abs(Math.round(Math.random()*displayWidth/2 - displayWidth/3)), y);
+            createPlatform(gameState.displayWidth/3 + Math.abs(Math.round(Math.random()*gameState.displayWidth/1.5 - gameState.displayWidth/2)), y);
         }
     }
 
-    const lowerPlatforms = () => {
-        
+    gameState.lowerPlatforms = () => {
+        removeBottomPlatform();
+        // You can now lose
+        gameState.player.setCollideWorldBounds(false);
+
+        // Loop lower each existing platform (must destroy and recreate)
+        gameState.platform.forEach((p) => {
+            p.y += gameState.p_inc;
+        });
+
+        // Static groups must be refreshed after moving.
+        gameState.platforms.refresh();
     }
 
     this.physics.add.collider(gameState.player, gameState.platforms);
-    buildPlatforms();
+    createPlatform(gameState.displayWidth/3 + Math.abs(Math.round(Math.random()*gameState.displayWidth/1.5 - gameState.displayWidth/2)), gameState.displayHeight-gameState.p_inc/2);
+    gameState.buildPlatforms();
 }
 
 function update(){
@@ -72,6 +97,11 @@ function update(){
 
     if (gameState.cursors.space.isDown && gameState.player.body.blocked.down){
         gameState.player.setVelocityY(-300);
+    }
+
+    if((gameState.player.body.y <= game.config.height - gameState.p_inc/1.5) && (gameState.cursors.space.isDown && gameState.player.body.blocked.down)){
+        gameState.lowerPlatforms();
+        gameState.buildPlatforms();
     }
 
 }
